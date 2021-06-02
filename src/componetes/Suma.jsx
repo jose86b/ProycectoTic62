7f4e47e0d703065e6d7e1423b6cd5c5657fba633
jose86b <jdominguezgordill@gmail.com>
2021-06-01 22:50:16 -0600

    practica3x2

--- src/componetes/Suma.jsx
+++ src/componetes/Suma.jsx
@@ -9,14 +9,23 @@ function Suma() {
     const [resultadod, setResultadod] = React.useState(0)
     const [Valor1, setValor1] = React.useState(0);
     const [Valor2, setValor2] = React.useState(0);
-    const Calcular = () => {
+    const Sumar = () => {
         console.log("Click")
         setResultados(parseInt(Valor1) + parseInt(Valor2));
-        setResultador(parseInt(Valor1) - parseInt(Valor2));
-        setResultadom(parseInt(Valor1) * parseInt(Valor2));
-        setResultadod(parseInt(Valor1) / parseInt(Valor2));
-        
     }
+    const Restar = () => {
+        console.log("Click")
+        setResultador(parseInt(Valor1) - parseInt(Valor2));
+    }
+    const Multiplicar = () => {
+        console.log("Click")
+        setResultadom(parseInt(Valor1) * parseInt(Valor2));
+    }
+    const Dividir = () => {
+        console.log("Click")
+        setResultadod(parseInt(Valor1) / parseInt(Valor2));
+    }
+    
     return (
         <Fragment>
             
@@ -26,7 +35,12 @@ function Suma() {
             <input type="number" value={resultador}/>
             <input type="number" value={resultadom}/>
             <input type="number" value={resultadod}/>
-            <button onClick={ () =>Calcular ()}>Calcular</button>
+            <button onClick={ () =>Sumar ()}>Sumar</button>
+            <button onClick={ () =>Restar ()}>Restar</button>
+            <button onClick={ () =>Multiplicar ()}>Multiplicar</button>
+            <button onClick={ () =>Dividir ()}>Dividir</button>
+
+
 
             
         </Fragment>
